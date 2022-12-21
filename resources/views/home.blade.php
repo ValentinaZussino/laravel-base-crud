75fa3b1f07e2dd257c60072de169010a0a99dcad
ValentinaZussino <vzussino@protonmail.com>
2022-12-21 20:05:44 +0100

    compact in routes giusto x provare immagini

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,12 @@
 @extends('layouts.app')
 @section('content')
-    <h1>santas list</h1>
+    {{-- <h1>{{$gift->image}}</h1> --}}
+    @foreach ($gifts as $item)
+        <img src="{{$item->image}}">
+        <br>
+        <span>{{$item->name}}</span>
+        <br>
+        <p>{{$item->address}}</p>
+        <p>{{$item->country}}</p>
+    @endforeach
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Gift;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    $gifts = Gift::all();
+    return view('home', compact('gifts'));
 })->name('home');
